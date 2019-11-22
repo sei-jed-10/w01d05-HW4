23c4e0446c920952b4de507c35508313ffc459c1
KOSaber <k.o.saber@hotmail.com>
2019-11-22 15:43:33 +0300

    almost done

---
+++ mbta.js
@@ -0,0 +1,57 @@
+function stopsBetweenStations(StartLine, StartStation, EndLine, EndStation){
+    const subways = [
+    {
+        line : "Red",
+        stations : ["South Station","Park Street","Kendall","Central","Harvard","Porter","Davis","Alewife"]
+    },
+    {
+        line : "Green",
+        stations : ["Government Center","Park Street","Boylston","Arlington","Copley","Hynes","Kenmore"]
+    },
+    {
+        line : "Orange",
+        stations : ["North Station","Haymarket","Park Street","State","Downtown Crossing","Chinatown","Back Bay","Forest Hills"]
+    }
+                    ];
+
+//    //bouns
+//   if(StartLine !== 'Red' && StartLine !== 'Green' && StartLine !== 'Orange' || EndLine !== 'Red' && EndLine !== 'Green' && EndLine !== 'Orange' )
+//   return "The line is wrong";
+
+//   //only accepts the correct stops for each of their stops.
+//  if( (EndLine == 'Red' )&& (EndStation !== "Alewife") || (EndLine == 'Green' )&& (EndStation !== "Kenmore") || (EndLine == 'Orange' )&& (EndStation !== "Forest Hills") )
+//  return "The last destination not in this line ";
+// //
+    
+      var indexofStartStation = 0;
+      var sLine = " ";
+      var indexofEndStation = 0;
+      var eLine = " ";
+      var sharedPoint = 0;
+      var sharedPoint2 = 0;
+      var totalSteps = 0;
+      
+     for( i = 0 ; i < subways.length ;i++){
+        if (StartLine == subways[i].line){
+         indexofStartStation = subways[i].stations[i].indexOf(StartStation);
+         sLine = subways[i].line;
+         sharedPoint =  subways[i].stations[i].indexOf("Park Street");
+                                 }
+        if (EndLine == subways[i].line){
+          indexofEndStation = subways[i].stations[i].indexOf(EndStation);
+          sharedPoint2 =  subways[i].stations[i].indexOf("Park Street");
+          eLine = subways[i].line;
+                                 }                          
+       
+     }
+     if(sLine == eLine)
+        totalSteps =  indexofEndStation - indexofStartStation;
+      else totalSteps = (indexofEndStation - sharedPoint2) + (sharedPoint-indexofStartStation);
+       
+    return totalSteps;
+    }
+    
+    
+    console.log(stopsBetweenStations('Red', 'Alewife', 'Red', 'Alewife')) // 0 stops
+    console.log(stopsBetweenStations('Red', 'Alewife', 'Red', 'South Station')) // 7 stops
+    console.log(stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore')) // 6 stops
