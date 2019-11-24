5714334fcfb3a609153b385031e901373fad748c
wardahmad <whitelight43@hotmail.com>
2019-11-24 22:58:14 +0300

    almostFinished

--- homework/mbta.js
+++ homework/mbta.js
@@ -3,36 +3,34 @@ const lineAndStations = {
   Green : ["Government Center","Park Street","Boylston","Arlington", "Copley","Hynes","Kenmore"],
   Orange : ["North Station", "Haymarket", "Park Street","State","Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"]
 };
-var concatArray = lineAndStations.Red.concat(lineAndStations.Green, lineAndStations.Orange); 
 
-var arr = []
 var result = 0;
 
-console.log(concatArray);
-
-//console.log(lineAndStations.Red[0].one)
 function stopsBetweenStations(startLine, startStation, endLine, endStation){
-
   var startL = Object.keys(lineAndStations).indexOf(startLine);
   var endL = Object.keys(lineAndStations).indexOf(endLine);
 
   var startS = lineAndStations[startLine].indexOf(startStation);
   var endS = lineAndStations[endLine].indexOf(endStation);
 
-  console.log(startL,startS,endL,endS);
+  var perkStar = lineAndStations[startLine].indexOf("Park Street");
+  var parkEnd = lineAndStations[endLine].indexOf("Park Street");
 
-  for (var j = startL; j <= endL; j++){
-    //result++;
-
-    for (var i = startS+1; i <=  endS/*proplem*/ ; i++){
-      console.log(lineAndStations[startLine][i]);
+  if (startLine === endLine){
+    for (var i = startS+1; i <= endS; i++ ){
       result++;
-
     }
+    
+  } else if (startLine !== endLine){
+    for (var j = startS+1; j <= perkStar; j++){ 
+      //console.log(lineAndStations[startLine][j]);
+      result++;
+    }
+    for (var m = parkEnd+1; m <= endS; m++){
+      //console.log(lineAndStations[startLine][m]);
+      result++;
+    } 
   }
-
-  return result;
+return result;
 }
-
-console.log(stopsBetweenStations('Red', 'South Station', 'Green', 'Copley') + " stops");
-
+console.log(stopsBetweenStations('Red', 'South Station', 'Green', 'Kenmore')+ " Stop");
