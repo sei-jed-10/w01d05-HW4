0c04c9edb56ed2df32723fe34aade9631db0344f
Abdulrahman Alsulami <a.sulami96@gmail.com>
2019-11-30 21:03:37 +0300

    finshed the HW

--- mbta.js
+++ mbta.js
@@ -1,77 +1,60 @@
- 
- const stations01 = {
- 
-    red :[
-       'South Station',
-       'Park Street',
-       'Kendall',
-       'Central',
-       'Harvard',
-       'Porter',
-       'Davis',
-       'Alewife']
-   ,
-   
-    green : [
-           'Government Center',
-           'Park Street',
-           'Boylston',
-           'Arlington',
-           'Copley',
-           'Hynes',
-           'Kenmore',
-            ]
-   
-   ,
-   
-    orange :  [
-      'North Station',
-      'Haymarket',
-      'Park Street',
-      'State',
-      'Downtown Crossing',
-      'Chinatown',
-      'Back Bay',
-      'Forest Hills',
-   ],
-   
-   stopsBetweenStations : function(a,b,c,d) {
-     let obj = 0;
-      for (let i in stations01){
-        if (a == stations01[i]){
-          for(let j = 0 ; j<stations01[i].length;j++){
-            if ( b == stations01[i][j]){
-            obj += 1; 
-   
-   
-            console.log(obj)
-            }
-          }
-        }
-      }
-   
-         for (let i in stations01){
-           let obj2 = 0;
-        if (c == stations01[i]){
-          for(let j = 0 ; j<stations01[i].length;j++){
-            obj += 1;
-            if (stations01[i][j] == d){
-              obj -=  stations01[i][j].length
-   
-            console.log(obj)
-            
-            }
-          }
-        }
-      }
-   
-   
-   
-   },
-   
-   
-   }
-    
-    stations01.stopsBetweenStations(stations01.red, 'Harvard', stations01.red, 'Alewife') ;
-   
-    
+
+const stations01 = {
+
+  red: [
+    'South Station',
+    'Park Street',
+    'Kendall',
+    'Central',
+    'Harvard',
+    'Porter',
+    'Davis',
+    'Alewife']
+  ,
+
+  green: [
+    'Government Center',
+    'Park Street',
+    'Boylston',
+    'Arlington',
+    'Copley',
+    'Hynes',
+    'Kenmore',
+  ]
+
+  ,
+
+  orange: [
+    'North Station',
+    'Haymarket',
+    'Park Street',
+    'State',
+    'Downtown Crossing',
+    'Chinatown',
+    'Back Bay',
+    'Forest Hills',
+  ],
+
+  stopsBetweenStations: function (startLine, startStation, endLine, endStation) {
+    //  if (startLine !== endLine){}
+    var strStation = this[startLine].indexOf(startStation);
+    var enStation = this[endLine].indexOf(endStation);
+    let br1 = this[startLine].indexOf("Park Street");
+    let br2 = this[endLine].indexOf("Park Street");
+    let firstStop = 0;
+    let secondStop =0;
+
+    if(startLine == endLine) {
+      result = Math.abs(strStation - enStation);
+    } else if (startLine != endLine) {
+        firstStop = Math.abs(strStation - br1);
+        firstStop = Math.abs(enStation - br2) + firstStop;
+
+    }
+      return result;
+  },
+
+
+}
+
+stations01.stopsBetweenStations("green", 'Copley', "orange", 'Chinatown');
