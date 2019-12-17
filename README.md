0bfaa285d85bae5427b68bc3119bdd83c12c208e
izahran <57293432+izahran@users.noreply.github.com>
2019-12-17 12:42:37 +0300

    Ibrahim Zahran

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-[![General Assembly Logo](https://camo.githubusercontent.com/1a91b05b8f4d44b5bbfb83abac2b0996d8e26c92/687474703a2f2f692e696d6775722e636f6d2f6b6538555354712e706e67)](https://generalassemb.ly/education/web-development-immersive)
 
 # JS Object Modeling
 
 ## Objectives
 
 - Practice with functions, arrays, and objects.
-
-## Instructions
-
-1. Fork and clone this repository.
-1. Create and checkout to a new branch, named `homework`.
-1. When finished, add, commit and push to your fork to submit a pull request.
 
 ## Activity
 
@@ -93,8 +86,4 @@ stopsBetweenStations('Red', 'South Station', 'Green', 'Copley')
 // Rider exits the train at Green Line and Copley.
 ```
 
-## [License](LICENSE)
 
-1. All content is licensed under a CC­BY­NC­SA 4.0 license.
-1. All software code is licensed under GNU GPLv3. For commercial use or
-    alternative licensing, please contact legal@ga.co.
--- mbta.js
+++ mbta.js
@@ -1,107 +1,54 @@
+//lines object with stops list
+var lines = {
+    'Red' : ['South Station', 'Kendall', 'Central','Harvard', 'Park Street', 'Porter', 'Davis', 'Alewife'],
+    'Green' : ['Government Center','Boylston', 'Arlington', 'Park Street', 'Copley', 'Hynes', 'Kenmore'],
+    'Orange' : ['North Station', 'Haymarket', 'State', 'Downtown Crossing', 'Chinatown', 'Park Street', 'Back Bay','Forest Hills']
+}
+
+//function to get total number of stops between two stations
+function stops_between_stations(start_line, start_station, end_line, end_station) {
+var stops = lines[start_line]		//get all stations for start line
+start_station_index = stops.indexOf(start_station) 		//get index for start_station
+common_station_index = stops.indexOf(getCommonStation())		//get index for intersected station for all subway lines
+start_diff = start_station_index - common_station_index		//#finding the index difference for start_station and intersected station,this will be
+                                                        //the total number of stops for the start line
+console.log('You must travel through the following stops on the ', start_line,' Line:')
+if(start_station_index <= common_station_index)  		//#if start_station_index is at 2 and common_station_index is 4, the range will be (2 to 4).
+stop_names = getStopNames(stops, start_station_index, common_station_index) 	//get stop names
+else
+stop_names = getStopNames(stops, common_station_index, start_station_index).reverse()
+console.log(stop_names)
+console.log('Change at ', getCommonStation(),'.')
+stops = lines[end_line]		//get all stations for end_station
+end_station_index = stops.indexOf(end_station)	//get end station index
+common_station_index = stops.indexOf(getCommonStation())
+end_diff = end_station_index - common_station_index //the total number of stops for the end line
+if(end_station_index <= common_station_index)
+stop_names = getStopNames(stops,end_station_index, common_station_index).reverse()   //get stops name with start and end range
+else
+stop_names = getStopNames(stops, common_station_index, end_station_index)
+console.log('Your trip continues through the following stops on the ', end_line, ' Line:')
+console.log(stop_names)
+//adding both start and end line stops count to get total number of stops count
+console.log(Math.abs(start_diff) + Math.abs(end_diff) , ' Stops in total')
+}
 
 
+//gets the stop names with the range of start and ends index
+function getStopNames(stops, start, ends) {
 
-const subwayLines =
-{
-  Red: ["south Station", "Park Street", "Kendall", "Central", "porter", "Davis", "Alewife"] ,
-  Green: ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"] ,
-  Orange: ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"],
+stop_names = []
+while(start <= ends) {
+stop_names.push(stops[start])
+start=start+1
+}
+return stop_names
+}
 
- stopsLoop: function(arr)
- { var stops =- 1;
-    for (let lines of arr)
-   {
-      stops++;
-   }var subwayLines =
-{
-  Red: ["south Station", "Park Street", "Kendall", "Central", "porter", "Davis", "Alewife"] ,
-  Green: ["Government Center", "Park Street", "Boylston", "Arlington", "Copley", "Hynes", "Kenmore"] ,
-  Orange: ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing", "Chinatown", "Back Bay", "Forest Hills"] ,
+//gets common intersected station name for all three lines
+function getCommonStation() {
+return 'Park Street'
+}
 
- Stops_loop: function(arr)
- { var stops =- 1;
-    for (let lines of arr)
-   {
-      stops++;
-   }
-   return stops;
- }
 
- stopsBetweenStations : function (startLine, startStation, endLine, endStation)
-  {
-    if((startLine || endLine) !== ("Red"||"Green"||"Orange"))
-    {
-    console.log("")
-    }
-    else
-    {
-    var stops=0;
-    var interSection_Index_1 = this[startLine].indexOf("Park Street");
-    var interSection_Index_2 = this[endLine].indexOf("Park Street");
-    var startStation_Index = this[startLine].indexOf(startStation);
-    var endStation_Index = this[endLine].indexOf(endStation);
-
-    if (startLine === endLine)
-    {
-      if(startStation === endStation)
-      {
-        console.log("Rider boards the train a " + startStation + " Station and end up in the same station .");
-        return "Num of stops = " + stops;
-      }
-      else if (startStation_Index<endStation_Index)
-     {
-       console.log("Rider boards the train a " + startLine + " Line and " + startStation + ".")
-        let arr = this[startLine].slice(startStation_Index , endStation_Index + 1);
-        console.log(arr);
-        var num = this.Stops_loop(arr);
-        console.log("Rider exits the train at " + endLine + " Line and" + endStation + " .");
-        return "Num of stops = " + num;
-      }
-      else if (startStation_Index > endStation_Index)
-     {
-       console.log("Rider boards the train a " + startLine + " Line and " + startStation + ".")
-        let arr = this[startLine].slice(endStation_Index ,  startStation_Index + 1);
-        console.log(arr);
-        var num = this.Stops_loop(arr);
-        console.log('Rider exits the train at " + endLine + " Line and " + endStation + " .");
-        return "Num of stops = " + num;
-      }
-    }
-   else
-    {
-     console.log("Rider boards the train a " + startLine + "Line and " + startStation + ".")
-     let arr1 = this[startLine].slice(startStation_Index, Intersection_Index_1 + 1);
-     let arr2 = this[endLine].slice(Intersection_Index_2, endStation_Index + 1);
-     var num1=this.Stops_loop(arr1);
-     console.log("Rider transfers from " + startLine + " Line to " + endLine +"Line at Park Street. ");
-     var num2=this.Stops_loop(arr2);
-     var num=num1+num2;
-     console.log("Rider exits the train at " + endLine +  Line and "+ endStation + " .");
-     return "Num of stops = " + num;
-    }
-  }
-};
-   return stops;
- }
-
- stopsBetweenStations : function (startLine, startStation, endLine, endStation)
-  {
-    if((startLine || endLine) !== ("Red"||"Green"||"Orange"))
-    {
-    console.log("")
-    }
-    else
-    {
-    var stops=0;
-    var interSection_Index_1 = this[startLine].indexOf("Park Street");
-    var interSection_Index_2 = this[endLine].indexOf("Park Street");
-    var startStation_Index = this[startLine].indexOf(startStation);
-    var endStation_Index = this[endLine].indexOf(endStation);
-
-    if (startLine === endLine)
-    {
-      if(startStation === endStation)
-      
-    }
-  }
-};
+stops_between_stations('Red', 'Central', 'Orange', 'Chinatown')
